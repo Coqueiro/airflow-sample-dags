2e6297d6f5783d95bdf5cf0fe254b7ced4781e07
Lucas Rocha Garcia <lucas.garcia@LGarcia-Mac-01.local>
2020-07-28 14:12:22 -0300

    Adding to use spark kubernetes operator from local copy

--- airflow_local/providers/cncf/kubernetes/example_dags/example_spark_kubernetes.py
+++ airflow_local/providers/cncf/kubernetes/example_dags/example_spark_kubernetes.py
@@ -20,6 +20,7 @@ This is an example DAG which uses SparkKubernetesOperator and SparkKubernetesSen
 In this example, we create two tasks which execute sequentially.
 The first task is to submit sparkApplication on Kubernetes cluster(the example uses spark-pi application).
 and the second task is to check the final state of the sparkApplication that submitted in the first state.
+
 Spark-on-k8s operator is required to be already installed on Kubernetes
 https://github.com/GoogleCloudPlatform/spark-on-k8s-operator
 """
@@ -30,8 +31,8 @@ from datetime import timedelta
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 # Operators; we need this to operate!
-from airflow_local.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow_local.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from airflow.utils.dates import days_ago
 
 # [END import_module]
@@ -42,7 +43,6 @@ from airflow.utils.dates import days_ago
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    # 'start_date': days_ago(1),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -53,25 +53,26 @@ default_args = {
 # [START instantiate_dag]
 
 dag = DAG(
-    'hello_world',
+    'spark_pi',
     default_args=default_args,
     description='submit spark-pi as sparkApplication on kubernetes',
-    schedule_interval=None,
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(1),
 )
 
 t1 = SparkKubernetesOperator(
-    task_id='hello_world',
-    namespace="spark",
-    application_file="hello_world.yaml",
+    task_id='spark_pi_submit',
+    namespace="default",
+    application_file="example_spark_kubernetes_spark_pi.yaml",
     kubernetes_conn_id="kubernetes_default",
     do_xcom_push=True,
     dag=dag,
 )
 
 t2 = SparkKubernetesSensor(
-    task_id='hello_world_monitor',
-    namespace="spark",
-    application_name="{{ task_instance.xcom_pull(task_ids='hello_world')['metadata']['name'] }}",
+    task_id='spark_pi_monitor',
+    namespace="default",
+    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
     kubernetes_conn_id="kubernetes_default",
     dag=dag
 )
